df7591fc59025ec08803730c83771b3a94446769
tangxueyi <2914512105.com>
2024-03-15 23:30:44 +0800

    测试

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld msg="三维陶瓷网站"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <iframe src="//player.bilibili.com/player.html?aid=771036439&bvid=BV1dr4y1j7j9&cid=778743560&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+iframe {
+position: fixed;
+right: 0;
+min-width: 100%;
+min-height: 100%;
+width: auto;
+height: auto;
+z-index: -999;
+}
+
+</style>
